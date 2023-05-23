983b6b9909ede75319ba76746e545c9187e530a7
Alexander Pyslarash <alex.pyslarash@gmail.com>
2023-05-23 12:33:01 -0400

    Started the form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/header';
+import Input from './components/input';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Input />
     </div>
   );
 }
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -35,7 +35,7 @@ export default function Header() {
       <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
         <div className="flex lg:flex-1">
           <a href="#" className="-m-1.5 p-1.5">
-            <span className="sr-only">Your Company</span>
+            <span className="sr-only">Not Your Average Logistics</span>
             <img className="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
           </a>
         </div>
